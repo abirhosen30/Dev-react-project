544a004d124f2cf7d9a46972fcc6da729608e42f
Abir30hosen <abir30hosen2005@gmail.com>
2026-09-13 15:41:17 +0600

    Mobile nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,15 @@
-import React from "react";
 import logo from "../assets/logo-text.png";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Nav = () => {
   return (
     <div className="container mx-auto flex items-center justify-between mt-5 pb-3 sticky top-0 z-50 bg-white">
+      <RxHamburgerMenu className="md:hidden" />
       <div>
-        <img src={logo} alt="" className="w-full" />
+        <img src={logo} alt="" className="h-8 w-auto sm:h-9" />
       </div>
 
-      <div>
+      <div className="hidden md:block">
         <ul className="flex justify-around gap-6 items-center text-center">
           <li>
             <a href="">Home</a>
@@ -30,7 +31,7 @@ const Nav = () => {
 
       <div className=" flex text-center items-center gap-4 ">
         <button className="font-semibold">Sign In</button>
-        <button className="border-2 bg-pink-600 text-white py-2 rounded-4xl px-4 font-semibold">
+        <button className="border-2 bg-pink-600 text-white py-2 rounded-4xl px-4 font-semibold text-sm">
           Sign Up
         </button>
       </div>
